b56c42aef500354052c8d5dd16fe37c9ca16e831
Legoben <legoben1998@gmail.com>
2014-12-07 16:38:05 -0600

    formatting improvements

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,21 @@
-
-	$(document).ready(function(){
-		var owner = "fossasia";
-		var repo = "fossasia.github.io";
-		$.ajax({
-			url: "http://api.github.com/repos/"+owner+"/"+repo+"/contributors",
-			dataType: "jsonp",
-			success: function(data){
-				data = data.data;
-				var n = data.length;
-				for(var i = 0; i < n; i++)
-				{
-					//Please, sorry for the next lines (>_<)
-					var str;
-					str = '<div class="sympl onesixth"><section class="person person--student"><div class="person__img">';
-					str+='<div class="person__img__info"><div class="person__img__info__cont">';
-					str += '<a target="_blank" href="'+data[i].html_url+'"><i class="icon-github"></i></a></div></div>';
-					str+='<img src="'+data[i].avatar_url+'"></div><h1 class="person__name">'+data[i].login+'</h1></section></div>';
-					$("#contr").append(str);
-				}
-			}
-		})
-	});
+$(document).ready(function () {
+    var owner = "fossasia";
+    var repo = "fossasia.github.io";
+    $.ajax({
+        url: "http://api.github.com/repos/" + owner + "/" + repo + "/contributors",
+        dataType: "jsonp",
+        success: function (data) {
+            data = data.data;
+            var n = data.length;
+            for (var i = 0; i < n; i++) {
+                //Please, sorry for the next lines (>_<)
+                var str;
+                str = '<div class="sympl onesixth"><section class="person person--student"><div class="person__img">';
+                str += '<div class="person__img__info"><div class="person__img__info__cont">';
+                str += '<a target="_blank" href="' + data[i].html_url + '"><i class="icon-github"></i></a></div></div>';
+                str += '<img src="' + data[i].avatar_url + '"></div><h1 class="person__name">' + data[i].login + '</h1></section></div>';
+                $("#contr").append(str);
+            }
+        }
+    })
+});
