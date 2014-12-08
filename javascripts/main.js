269a66b02b4b7e8074f79cd1e42f36338a0ffec1
Namanyay Goel <mail@namanyayg.com>
2014-12-08 18:23:35 +0530

    Fixing layout of 'contributing' section
    Onesixth turned to quarter, better font size for names

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             for (var i = 0; i < n; i++) {
                 //Please, sorry for the next lines (>_<)
                 var str;
-                str = '<div class="sympl onesixth"><section class="person person--student"><div class="person__img">';
+                str = '<div class="sympl quarter"><section class="person person--contributer"><div class="person__img">';
                 str += '<div class="person__img__info"><div class="person__img__info__cont">';
                 str += '<a target="_blank" href="' + data[i].html_url + '"><i class="icon-github"></i></a></div></div>';
                 str += '<img src="' + data[i].avatar_url + '"></div><h1 class="person__name">' + data[i].login + '</h1></section></div>';
