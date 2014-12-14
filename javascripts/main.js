2a288e99226b87b36c28ed45ab021d393abac2f0
Namanyay Goel <mail@namanyayg.com>
2014-12-14 18:11:16 +0530

    Removing duplicate code on Twitter

--- javascripts/main.js
+++ javascripts/main.js
@@ -47,6 +47,3 @@ function aniScroll(id){
 	new WOW().init();
 
 })(jQuery);
-
-// Twitter feed
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
