bc0597a183efbddd0f4c529f8bdebe62e16001da
Samarjeet <singh.samarjeet.27@gmail.com>
2014-12-22 19:25:09 +0530

    Better way to build contributors list

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,15 @@
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) { 
+          return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+          ;
+        });
+    };
+}
+
 $(document).ready(function () {
 
     //
@@ -25,12 +37,11 @@ $(document).ready(function () {
             data = data.data;
             var n = data.length;
             for (var i = 0; i < n; i++) {
-                //Please, sorry for the next lines (>_<)
-                var str;
-                str = '<div class="sympl quarter"><section class="person person--contributer"><div class="person__img">';
-                str += '<div class="person__img__info"><div class="person__img__info__cont">';
-                str += '<a target="_blank" href="' + data[i].html_url + '"><i class="icon-github"></i></a></div></div>';
-                str += '<img src="' + data[i].avatar_url + '"></div><h1 class="person__name">' + data[i].login + '</h1></section></div>';
+                var str = '<div class="sympl quarter"><section class="person person--contributer"><div class="person__img"><div class="person__img__info"><div class="person__img__info__cont"><a target="_blank" href="{0}"><i class="icon-github"></i></a></div></div><img src="{1}"></div><h1 class="person__name">{2}</h1></section></div>'.format(data[i].html_url, data[i].avatar_url, data[i].login)
+                // str = '<div class="sympl quarter"><section class="person person--contributer"><div class="person__img">';
+                // str += '<div class="person__img__info"><div class="person__img__info__cont">';
+                // str += '<a target="_blank" href="' + data[i].html_url + '"><i class="icon-github"></i></a></div></div>';
+                // str += '<img src="' + data[i].avatar_url + '"></div><h1 class="person__name">' + data[i].login + '</h1></section></div>';
                 $("#contr").append(str);
             }
         }
