2008c0e4d65403b01f723b8e39344c9dc60d1be9
Namanyay Goel <mail@namanyayg.com>
2015-01-01 01:53:19 +0530

    Added explanation to imageSize.js

--- tests/imageSize.js
+++ tests/imageSize.js
@@ -1,9 +1,23 @@
+/**
+ * imageSize
+ * a test to check ratios of images
+ * by Namanyay Goel
+ *
+ * The test has a simple idea - it loads all
+ * images from the images/students and
+ * images/mentors folders, and compares
+ * their heights and widths. If they do 
+ * not match, the test fails.
+ */
+
+
 var fs = require('fs');
 var sizeOf = require('image-size');
 var assert = require('assert');
 
 function isImagesRatioEqual (path) {
   var result = true;
+  // Reads directories
   fs.readdir(path, function(err, files) {
     if ( err ) {
       throw err;
@@ -11,9 +25,11 @@ function isImagesRatioEqual (path) {
     }
 
     files.forEach(function(file, i) {
+      // Checks extension to know if it's an image or not
       var ext = file.split('.')[ file.split('.').length - 1 ].toLowerCase();
       if ( ext == 'jpg' || ext == 'png' || ext == 'gif' ) {
         var dimensions = sizeOf(path + file);
+        // Asserts dimensions
         assert.equal(dimensions.width, dimensions.height)
         if ( dimensions.width !== dimensions.height ) {
           result = false;
