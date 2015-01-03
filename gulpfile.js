66ba218e07dc758f954e51262cc779b044013561
Namanyay Goel <mail@namanyayg.com>
2015-01-03 17:35:01 +0530

    Installing gulp and configuring travis

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
+var gulp = require('gulp')
 var scsslint = require('gulp-scss-lint');
 
 gulp.task('scss-lint', function() {
-  gulp.src('/scss/*.scss')
+  gulp.src('/stylesheets/**/*.scss')
     .pipe(scsslint());
 });
