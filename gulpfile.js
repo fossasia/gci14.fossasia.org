fee8c02c50679f7c35a2d418e3ac68042d010f18
Namanyay Goel <mail@namanyayg.com>
2015-01-03 20:41:54 +0530

    More fixes to gulp and scss configuration, fixing files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp')
 var scsslint = require('gulp-scss-lint');
 
 gulp.task('scss-lint', function() {
-  gulp.src('./stylesheets/**/*.scss')
-    .pipe(scsslint());
+  gulp.src(['./stylesheets/**/*.scss', '!./stylesheets/partials/_animate.scss'])
+    .pipe(scsslint({
+      config: 'scss-lint-config.yml'
+    }));
 });
