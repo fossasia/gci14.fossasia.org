0e22b5b5b81bb204f06ec981d1f04a6a73edab8c
Namanyay Goel <mail@namanyayg.com>
2015-01-04 01:06:51 +0530

    Adding JSHint to lint JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp')
 var scsslint = require('gulp-scss-lint');
+var jshint = require('gulp-jshint')
 
 gulp.task('scss-lint', function() {
   gulp.src(['./stylesheets/**/*.scss', '!./stylesheets/external/*', '!./stylesheets/stylesheet.scss'])
     .pipe(scsslint({
       config: 'scss-lint-config.yml'
     }));
+});
+
+gulp.task('js-lint', function() {
+  gulp.src('./javascripts/main.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
