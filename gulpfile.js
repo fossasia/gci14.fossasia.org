0a4c34b91433224a523ad7ff4238d78c12cb0c08
Namanyay Goel <mail@namanyayg.com>
2015-01-03 20:47:56 +0530

    Moving external SCSS files to 'external' folder.
    The external folder is ignored by SCSS lint. Stylesheet.scss
    now merely includes files and is ignored by scss lint, too.
    All changes go in _main.scss file for the future.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var scsslint = require('gulp-scss-lint');
 
 gulp.task('scss-lint', function() {
-  gulp.src(['./stylesheets/**/*.scss', '!./stylesheets/partials/_animate.scss', '!./stylesheets/stylesheet.scss'])
+  gulp.src(['./stylesheets/**/*.scss', '!./stylesheets/external/*', '!./stylesheets/stylesheet.scss'])
     .pipe(scsslint({
       config: 'scss-lint-config.yml'
     }));
